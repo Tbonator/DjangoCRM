4b0954cd08e442587e655b8c697807734bf891f0
Tbonator <thabo2g5@yahoo.ca>
2021-04-06 23:38:50 +0200

    Adding login logic and error messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,30 +1,48 @@
+from django.contrib.auth import authenticate, login
 from accounts.models import Customers, Order, Product
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, request
 from django.forms import inlineformset_factory
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import SetPasswordForm, UserCreationForm
+
+from django.contrib import messages
+
 
 # Create your views here.
 def registerPage(request):
-    form = CreateUserForm()
-    
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-        
-        
-    context = {
-        'form':form
-    }
-    return render(request, 'accounts/register.html',context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
+
 
 def loginPage(request):
-    context = {}
-    return render(request, 'accounts/login.html',context)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):
@@ -58,7 +76,7 @@ def customer(request, pk):
 
     orders = customer.order_set.all()
     orders_count = orders.count()
-    myFilter = OrderFilter(request.GET,queryset=orders)
+    myFilter = OrderFilter(request.GET, queryset=orders)
     orders = myFilter.qs
 
     context = {
@@ -66,7 +84,7 @@ def customer(request, pk):
         'orders': orders,
         'orders_count': orders_count,
         'myFilter': myFilter
-    
+
     }
     return render(request, "accounts/customer.html", context)
 
